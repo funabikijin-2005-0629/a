6ab73ffbdb3cadc96466be7847d693703af25b92
funabikijin-2005-0629 <e1w24090@oit.ac.jp>
2024-10-16 18:07:18 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+radio.onReceivedNumber(function (receivedNumber) {
+	
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+})
+radio.setGroup(1)
 basic.forever(function () {
 	
 })
